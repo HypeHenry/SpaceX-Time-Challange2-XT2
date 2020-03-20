51559fad9d6ff6c7ca1410e76c30830a96574816
Henry Barima <h.barima@me.com>
2020-03-20 21:50:59 +0100

    Done javascript

--- js/time.js
+++ js/time.js
@@ -1,11 +1,12 @@
 function showSpaceTime() {
+
     let date = new Date();
     let h = date.getHours(); // van 0 tot 23
     let m = date.getMinutes(); // van 0 tot 59
     let s = date.getSeconds(); // 0 van 59
     let sessie = "AM";
 
-    if(h ==0) {
+    if (h == 0) {
         h = 12;
     }
 
@@ -18,9 +19,37 @@ function showSpaceTime() {
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
 
-    let time = h + ":" + m + ":" + s + " " + sessie;
-    document.getElementById("mySpaceDisplay").innerText = time;
+    switch (new Date().getDay()) { // case switch van welke dag het is
+        case 0:
+            dag = "Zondag";
+            break;
+        case 1:
+            dag = "Maandag";
+            break;
+        case 2:
+            dag = "Dinsdag";
+            break;
+        case 3:
+            dag = "Woensdag";
+            break;
+        case 4:
+            dag = "Donderdag";
+            break;
+        case 5:
+            dag = "Vrijdag";
+            break;
+        case 6:
+            dag = "Zaterdag";
+            break;
+        default:
+            dag = "Tijd:";
+            break;
+    }
+
+    let time = dag +" " + h + ":" + m + ":" + s + " " + sessie ;
+    document.getElementById("MySpaceDisplay").innerText = time;
     document.getElementById("MySpaceDisplay").textCont = time;
 
     setTimeout(showSpaceTime, 1000);
 }
+showSpaceTime();

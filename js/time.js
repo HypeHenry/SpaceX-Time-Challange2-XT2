e8bab982a9b34c7067a69aca376746e8d9871fe2
Henry Barima <h.barima@me.com>
2020-03-20 13:19:29 +0100

    gitignore

--- js/time.js
+++ js/time.js
@@ -1,23 +1,18 @@
-document.addEventListener("DOMContentLoaded", () =>
-    requestAnimationFrame(updateTime)
-);
+function showSpaceTime() {
 
-function updateTime() {
-    document.documentElement.style.setProperty(
-        "--timer-day",
-        "'" + moment().format("dd") + "'"
-    );
-    document.documentElement.style.setProperty(
-        "--timer-hours",
-        "'" + moment().format("k") + "'"
-    );
-    document.documentElement.style.setProperty(
-        "--timer-minutes",
-        "'" + moment().format("mm") + "'"
-    );
-    document.documentElement.style.setProperty(
-        "--timer-seconds",
-        "'" + moment().format("ss") + "'"
-    );
-    requestAnimationFrame(updateTime);
+    let date = new date();
+    let h = date.getHours(); // van 0 tot 23
+    let m = date.getMinutes(); // van 0 tot 59
+    let s = date.getSeconds(); // 0 van 59
+    let sessie = "AM";
+
+    if (h == 0){
+        h = 12;
+    }
+
+    if (h > 12){
+        h = h - 12;
+        sessie = "PM";
+    }
+
 }

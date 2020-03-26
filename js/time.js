090befeb42cd06be8f16a8b569d4d562d353de15
Henry Barima <h.barima@me.com>
2020-03-26 16:03:32 +0100

    Done javascript

--- js/time.js
+++ js/time.js
@@ -1,19 +1,21 @@
 function showSpaceTime() {
 
     let date = new Date();
-    let h = date.getHours(); // van 0 tot 23
-    let m = date.getMinutes(); // van 0 tot 59
-    let s = date.getSeconds(); // 0 van 59
-    let sessie = "AM";
+    let h = date.getHours(); // haal van 0 tot 23
+    let m = date.getMinutes(); // haal van 0 tot 59
+    let s = date.getSeconds(); // haal 0 van 59
+    
+    // Weet niet of ik AM/PM ga gebruiken door die bug
+    // let sessie = "AM";
 
-    if (h === 0) {
-        h = 12;
-    }
-
-    if (h > 12) {
-        h = h - 12;
-        sessie = "PM";
-    }
+    // if (h === 0) {
+    //     h = 12;
+    // }
+    //
+    // if (h > 12){
+    //     h = h - 12;
+    //     sessie = "PM";
+    // }
 
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
@@ -46,10 +48,33 @@ function showSpaceTime() {
             break;
     }
 
-    let time = dag +" " + h + ":" + m + ":" + s + " " + sessie ;
+    let time = dag +" " + h + ":" + m + ":" + s + " " ;
     document.getElementById("MySpaceDisplay").innerText = time;
     document.getElementById("MySpaceDisplay").textCont = time;
 
+
+
+
+    if (h >= 7 && h < 12) {
+        document.getElementsByTagName("video").style.display = "none";
+    }
+    else if (h >= 12 && h < 18) {
+        document.getElementsByTagName("video").style.display = "none";
+
+    }
+    else {
+        document.getElementsByTagName("video").style.display = "none";
+    }
     setTimeout(showSpaceTime, 1000);
+
+    //
+    // if (h === 0) {
+    //     h = 12;
+    // }
+    //
+    // if (h > 12){
+    //     h = h - 12;
+    //     sessie = "PM";
+    // }
 }
 showSpaceTime();
